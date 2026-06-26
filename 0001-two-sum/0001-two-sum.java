b382c8e005c0b7ed296312d09eabe3fdd3a4786f
Devyani Gaikwad <gaikwaddevyani06@gmail.com>
2026-06-26 19:02:08 +0530

    Time: 3 ms (56.5%), Space: 47.1 MB (39.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,35 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int[nums.length];
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i=0; i<nums.length; i++) {
-            arr[i] = i;
-        }
-        sort(nums, arr);
-        int low = 0; int high = nums.length-1;
-        while (low<high) {
-            if (nums[low]+nums[high] == target) {
-                return new int[]{arr[low], arr[high]};
-            } else if (nums[low]+nums[high] > target) {
-                high--;
-            } else {
-                low++;
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
-        return new int[]{arr[low], arr[high]};
-    }
-    public static void sort(int[] nums, int[] arr) {
-        for (int i=0; i<nums.length; i++) {
-            for (int j=0; j<nums.length-1-i; j++) {
-                if(nums[j] > nums[j+1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-
-                    int temp2 = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp2;
-                }
-            }
-        }
+        return new int[] {};
     }
 }
